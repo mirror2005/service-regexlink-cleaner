555360658aceda14e1045ade2c3fe459cf24f84c
Ashik Muhammed <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-12-17 15:19:51 +0530

    Update service.py

--- service.py
+++ service.py
@@ -23,7 +23,7 @@ async def start(bot, message):
         disable_web_page_preview=True,
         reply_markup=START_BUTTON
     )
-@Client.on_message(filters.regex("http") | filters.regex("t.me") | filters.regex("youtu.be") | filters.regex("https") | filters.regex("/" ) | filters.service)
+@Client.on_message(filters.regex("http") | filters.regex("t.me") | filters.regex("youtu.be") | filters.regex("com") | filters.regex("https") | filters.regex("/" ) | filters.service)
 async def delete(bot,message):
  await message.delete()
 
